c5b83d92d6623e8291b60a713e3b6cf61e45a5bf
Cuong-FrontEnd <cuong@codeforcevina.com>
2017-05-31 13:57:41 +0700

    add route sign out

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+
+  devise_for :users do
+      get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #set route path
   root 'articles#index'
